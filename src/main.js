143f5eea28ef844047d9bf4cbd5a5d467060865c
Khlebnikov Anton <sg@ooos.ru>
2020-08-31 12:25:09 +0300

    init

---
+++ src/main.js
@@ -0,0 +1,27 @@
+const cart = document.getElementById('cart');
+const modal = document.getElementById('modal');
+const checkout = document.getElementById('checkout');
+const changeStep = document.getElementsByClassName('change-step');
+
+const onChangeStep = (step) => {
+  switch (step) {
+    case 'cart':
+      cart.classList.add('active');
+      checkout.classList.remove('active');
+      modal.scroll(0, 0);
+      break;
+  
+    case 'checkout':
+      checkout.classList.add('active');
+      cart.classList.remove('active');
+      modal.scroll(0, 0);
+      break;
+  }
+}
+
+for (let element of changeStep) {
+  element.addEventListener("click", function () {
+    onChangeStep(element.dataset.step);
+  }, false);
+}
+
